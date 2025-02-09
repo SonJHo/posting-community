07db825f891e9a5b7e0e76af342e1ef18dce3427
sy <ssyeo_n@naver.com>
2025-02-10 02:42:43 +0900

    loginForm UI 수정

--- src/main/kotlin/com/myproject/postproject/controller/LoginForm.kt
+++ src/main/kotlin/com/myproject/postproject/controller/LoginForm.kt
@@ -4,10 +4,10 @@ import jakarta.validation.constraints.NotEmpty
 import jakarta.validation.constraints.Pattern
 
 class LoginForm {
-    @NotEmpty(message = "id를 입력하세요")
+    @NotEmpty
     var accountId : String? = null
 
 
-    @NotEmpty(message = "pw를 입력하세요")
+    @NotEmpty
     var password : String? = null
 }
